49a7f7ffea38bd9d1bb1240ec45215cd3caf8acb
KazTom <kazuhito1oo1@gmail.com>
2024-11-18 19:28:43 +0900

    カメラと平面のスケール調整

--- src/controllers/ThreeFunctional.ts
+++ src/controllers/ThreeFunctional.ts
@@ -17,7 +17,21 @@ export default class ThreeFunctional extends Controller {
     this.ripple();
   }
 
-  private ripple() {
+  private calculateAspectRatio(width: number, height: number) {
+    console.log(width, height);
+    // 最大公約数を求める関数
+    const gcd = (a: number, b: number): number => {
+      return b === 0 ? a : gcd(b, a % b);
+    };
+
+    const divisor = gcd(width, height);
+    const simplifiedWidth = width / divisor;
+    const simplifiedHeight = height / divisor;
+
+    return [simplifiedWidth, simplifiedHeight];
+  }
+
+  private async ripple() {
     const rootElement = document.getElementById('app');
 
     // シーン、カメラ、レンダラーのセットアップ
@@ -39,9 +53,22 @@ export default class ThreeFunctional extends Controller {
     rootElement?.appendChild(renderer.domElement);
 
     // 画像テクスチャのロード
+    const img = new Image();
+    img.src = TextureImage;
+    const loadTextureImg = () =>
+      new Promise<HTMLImageElement>((resolve, reject) => {
+        img.onload = () => {
+          resolve(img);
+        };
+      });
+    await loadTextureImg();
+
     const texture = new THREE.TextureLoader().load(TextureImage);
+    const [simplifiedWidth, simplifiedHeight] = this.calculateAspectRatio(img.width, img.height);
+    console.log(simplifiedWidth, simplifiedHeight);
 
     // ポインター情報を保存
+    const pointerPosition = new THREE.Vector2(-10, -10);
     const previousPointerPosition = new THREE.Vector2(-10, -10);
     let pointerSpeed = 0; // ポインター速度
 
@@ -49,7 +76,7 @@ export default class ThreeFunctional extends Controller {
     const uniforms: THREE.ShaderMaterialParameters['uniforms'] = {
       u_time: { value: 0 },
       u_texture: { value: texture },
-      u_mouse: { value: previousPointerPosition },
+      u_mouse: { value: pointerPosition },
       u_mouseIntensity: { value: 0 }, // 減衰効果用の強度
     };
     const material = new THREE.ShaderMaterial({
@@ -59,25 +86,47 @@ export default class ThreeFunctional extends Controller {
     });
 
     // 平面ジオメトリの作成
-    const geometry = new THREE.PlaneGeometry(3, 3);
+    const geometry = new THREE.PlaneGeometry(1, 1);
     const mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
+
+    const helper = new THREE.CameraHelper(camera);
+    scene.add(helper);
 
     // リサイズイベント
     const onResize = () => {
       const width = rootElement?.clientWidth ?? 1;
       const height = rootElement?.clientHeight ?? 1;
 
-      const aspect = width / height;
-
+      // レンダラーのサイズを更新
       renderer.setPixelRatio(window.devicePixelRatio);
       renderer.setSize(width, height);
 
-      camera.left = -aspect;
-      camera.right = aspect;
-      camera.top = 1;
-      camera.bottom = -1;
+      // 画面のアスペクト比と画像のアスペクト比を計算
+      const aspect = width / height;
+      const imgAspect = img.width / img.height;
+
+      // カメラの投影範囲を更新
+      if (aspect > imgAspect) {
+        // 横長の画面の場合
+        camera.left = -aspect;
+        camera.right = aspect;
+        camera.top = 1;
+        camera.bottom = -1;
+      } else {
+        // 縦長の画面の場合
+        camera.left = -1;
+        camera.right = 1;
+        camera.top = 1 / aspect;
+        camera.bottom = -1 / aspect;
+      }
       camera.updateProjectionMatrix();
+
+      // `PlaneGeometry`のサイズをカメラの投影範囲に合わせる
+      const planeWidth = Math.abs(camera.right - camera.left);
+      const planeHeight = Math.abs(camera.top - camera.bottom);
+
+      mesh.scale.set(planeWidth, planeHeight, 1); // 平面をカメラの投影範囲にスケール
     };
     onResize();
 
@@ -91,31 +140,25 @@ export default class ThreeFunctional extends Controller {
       const uvY = 1.0 - y;
 
       // 現在のポインター位置
-      const currentPointerPosition = new THREE.Vector2(uvX, uvY);
+      pointerPosition.set(uvX, uvY);
+    };
+    renderer.domElement?.addEventListener('pointermove', onPointerMove);
 
-      // 移動速度を計算
-      pointerSpeed = currentPointerPosition.distanceTo(previousPointerPosition);
+    // フレーム毎の再描画
+    const tick = () => {
+      // マウスの移動速度を計算
+      pointerSpeed = pointerPosition.distanceTo(previousPointerPosition);
 
-      // マウス位置と強度を更新
-      material.uniforms.u_mouse.value.x = uvX;
-      material.uniforms.u_mouse.value.y = uvY;
-
-      // 移動速度に基づいて強度を増加
+      // マテリアルを更新
+      material.uniforms.u_mouse.value.copy(pointerPosition);
       material.uniforms.u_mouseIntensity.value += pointerSpeed * 0.5; // 0.5は速度に基づく強度スケール
       material.uniforms.u_mouseIntensity.value = Math.min(
         material.uniforms.u_mouseIntensity.value,
         1.0,
       ); // 最大値を制限
 
-      // 現在の位置を次回の比較用に保存
-      previousPointerPosition.copy(currentPointerPosition);
-    };
-    rootElement?.addEventListener('pointermove', onPointerMove);
-
-    // フレーム毎の再描画
-    const tick = () => {
+      // 時間を更新
       material.uniforms.u_time.value += 0.05;
-      renderer.render(scene, camera);
 
       // 強度を徐々に減衰
       if (material.uniforms.u_mouseIntensity.value > 0.0) {
@@ -125,6 +168,10 @@ export default class ThreeFunctional extends Controller {
         }
       }
 
+      // 前回の位置を更新
+      previousPointerPosition.copy(pointerPosition);
+
+      renderer.render(scene, camera);
       requestAnimationFrame(tick);
     };
     tick();
